29a18f076b5fad03275e88b0842ab95b20df51e8
Alexander <a@a>
2024-12-22 02:25:45 +0300

    Reformat code

--- src/entityCollection/dao/EntityCollection.java
+++ src/entityCollection/dao/EntityCollection.java
@@ -3,7 +3,7 @@ package entityCollection.dao;
 import entityCollection.model.Entity;
 
 public interface EntityCollection {
-    public void add(Entity entity);
+    void add(Entity entity);
 
-    public Entity removeMaxValue();
+    Entity removeMaxValue();
 }
